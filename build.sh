adca1922d0322a9aaf2f0170e8956cf1b0e6c356
hanedotoaranea <arachnoideaqinki@gmail.com>
2025-11-01 20:56:51 +0500

    hanedotoaranea

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-lb config -d bullseye \
+lb config -d trixie \
           --debian-installer live \
-          --debian-installer-distribution bullseye \
+          --debian-installer-distribution trixie \
           --debian-installer-gui true \
-          --archive-areas "main contrib non-free" \
-          --debootstrap-options "--variant=minbase" \
-
-          sudo lb build
+          --archive-areas "main contrib non-free non-free-firmware" \
+          --debootstrap-options "--variant=minbase"
+		  
+		  sudo lb build
